7b33f3a84e930b4d45ef487c106beb3673d6a530
Cheveux <katenasova364@mail.ru>
2024-03-07 12:47:58 +0300

    Добавьте файлы проекта.

---
+++ ИСРПО07.03.24/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ИСРПО07._03._24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            double result = CalculateFunction(number);
+
+            Console.WriteLine($"Результат вычисления функции: {result}");
+
+            Console.ReadKey();
+        }
+
+        static double CalculateFunction(double x)
+        {
+            return 3 * Math.Sin(x + 2);
+        }
+    }
+}
